fb9f71e8015a5f4da45b8295ddcf1343d1902eb6
t4sk <tsk.nakamura@gmail.com>
2025-05-28 16:04:17 +0900

    fix

--- merkle/src/bin/verify.rs
+++ merkle/src/bin/verify.rs
@@ -1,10 +1,41 @@
+use std::collections::HashSet;
 use std::env;
+use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::process::Command;
 
 use merkle::{calc_root, hash_val, hex_to_bytes32, verify};
 
+fn diff() {
+    let raw = Command::new("git")
+        .args(["show", "origin/main:data.txt"])
+        .output()
+        .unwrap()
+        .stdout;
+
+    let old_users: Vec<String> = String::from_utf8(raw)
+        .unwrap()
+        .lines()
+        .map(|s| str::trim(s).to_string())
+        .collect();
+
+    // Load current data
+    let raw = fs::read_to_string("data.txt").unwrap();
+    let new_users: Vec<String> = raw.lines().map(|s| str::trim(s).to_string()).collect();
+
+    assert!(
+        new_users.len() == old_users.len() + 1,
+        "Invalid number of users old: {:#?} new: {:#?}",
+        old_users,
+        new_users,
+    );
+}
+
 fn main() {
+    diff();
+    return;
+
     let args: Vec<String> = env::args().collect();
 
     let data_path = &args[1];
@@ -19,8 +50,6 @@ fn main() {
         hashes.push(hash_val(&v));
     }
 
-    let idx = hashes.len() - 1;
-
     let src = File::open(proof_path).unwrap();
     let reader = BufReader::new(src);
 
@@ -30,6 +59,7 @@ fn main() {
         proof.push(hex_to_bytes32(&v).unwrap());
     }
 
+    let idx = hashes.len() - 1;
     let root = proof[0];
     let leaf = proof[1];
 

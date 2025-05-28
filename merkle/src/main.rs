1af579f7dcffa1a8cd2e7f31c984e4e2ec7e037a
t4sk <tsk.nakamura@gmail.com>
2025-05-28 11:04:05 +0900

    merkle

---
+++ merkle/src/main.rs
@@ -0,0 +1,126 @@
+use ethers::core::abi::encode;
+use ethers::core::abi::Token;
+use ethers::types::H256;
+use ethers::utils::keccak256;
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn hash_val(val: &str) -> [u8; 32] {
+    let encoded = encode(&[Token::String(val.to_string())]);
+    keccak256(encoded)
+}
+
+fn hash(left: [u8; 32], right: [u8; 32]) -> [u8; 32] {
+    let mut a: H256 = left.into();
+    let mut b: H256 = right.into();
+
+    if b < a {
+        (a, b) = (b, a);
+    }
+
+    let encoded = encode(&[
+        Token::FixedBytes(a.as_bytes().to_vec()),
+        Token::FixedBytes(b.as_bytes().to_vec()),
+    ]);
+
+    keccak256(encoded)
+}
+
+fn min(x: usize, y: usize) -> usize {
+    if x < y {
+        x
+    } else {
+        y
+    }
+}
+
+fn calc_root_hash(hashes: &Vec<[u8; 32]>) -> [u8; 32] {
+    let mut hs = hashes.clone();
+    let mut n = hs.len();
+
+    while n > 1 {
+        for i in (0..n).step_by(2) {
+            let left = hs[i];
+            let right = hs[min(i + 1, n - 1)];
+            hs[i / 2] = hash(left, right);
+        }
+        // div by 2 and round up
+        // if n is even => n = n / 2
+        // else         => n = (n + 1) / 2
+        n = (n + (n & 1)) / 2;
+    }
+
+    hs[0]
+}
+
+fn gen_proof(hashes: &Vec<[u8; 32]>, idx: usize) -> Vec<[u8; 32]> {
+    let mut hs = hashes.clone();
+    let mut proof: Vec<[u8; 32]> = Vec::new();
+    let mut n = hs.len();
+    let mut k = idx;
+
+    while n > 1 {
+        //      1
+        //    /   \
+        //   2     3
+        //  / \   / \
+        // 4   5 6   7
+        let j = if k & 1 == 1 { k - 1 } else { min(k + 1, n - 1) };
+        proof.push(hs[j]);
+        k /= 2;
+
+        for i in (0..n).step_by(2) {
+            let left = hs[i];
+            let right = hs[min(i + 1, n - 1)];
+            hs[i / 2] = hash(left, right);
+        }
+        // div by 2 and round up
+        // if n is even => n = n / 2
+        // else         => n = (n + 1) / 2
+        n = (n + (n & 1)) / 2;
+    }
+
+    proof
+}
+
+fn verify(root: [u8; 32], proof: &Vec<[u8; 32]>, leaf_hash: [u8; 32]) -> bool {
+    let mut h = leaf_hash;
+
+    for i in 0..proof.len() {
+        h = hash(proof[i], h);
+    }
+
+    h == root
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let file = File::open(args[1].clone()).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut hashes: Vec<[u8; 32]> = Vec::new();
+    for line in reader.lines() {
+        let v: String = line.unwrap().parse().unwrap();
+        hashes.push(hash_val(&v));
+    }
+
+    let root = calc_root_hash(&hashes);
+    let idx = 7;
+    let proof = gen_proof(&hashes, idx);
+
+    let r: H256 = root.into();
+    println!("root {:?}", r);
+
+    let leaf: H256 = hashes[idx].into();
+    println!("leaf {:?}", leaf);
+
+    for p in proof.iter() {
+        let h: H256 = p.into();
+        println!("proof {:#?}", h);
+    }
+
+    let is_valid = verify(root, &proof, hashes[idx]);
+    println!("{:?}", is_valid);
+}

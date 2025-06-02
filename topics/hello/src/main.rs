57d272ea0b9b520667495b7cd8ca2316eaa28ba3
t4sk <tsk.nakamura@gmail.com>
2025-06-02 12:59:19 +0900

    hello

---
+++ topics/hello/src/main.rs
@@ -0,0 +1,12 @@
+// Attribute that tells the Rust compiler to suppress warnings
+// about unused code
+// Attributes - metadata for the compiler
+#![allow(unused)]
+
+// main() is the entry point of Rust program
+fn main() {
+    // println! is a macro that prints text to the console.
+    // Macros in Rust generate code at compile time and are
+    // invoked with an exclamation mark (!).
+    println!("Hello Rust");
+}

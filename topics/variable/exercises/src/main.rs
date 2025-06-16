6641715a1b1be34e7f8dca768d55b529313b20e8
t4sk <tsk.nakamura@gmail.com>
2025-06-16 13:18:25 +0900

    test

--- topics/variable/exercises/src/main.rs
+++ topics/variable/exercises/src/main.rs
@@ -2,7 +2,7 @@
 // Exercise: Fix the code to make it compile and pass the assertions
 
 fn main() {
-    // TODO: Make this variable mutable
+    // Exercise 1: Make this variable mutable
     let mut count = 1;
     count += 1;
 

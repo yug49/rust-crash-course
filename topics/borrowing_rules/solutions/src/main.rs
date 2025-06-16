923ac4642dcc998d82c686406fe702d905096f8d
t4sk <tsk.nakamura@gmail.com>
2025-06-17 06:38:30 +0900

    exercise

--- topics/borrowing_rules/solutions/src/main.rs
+++ topics/borrowing_rules/solutions/src/main.rs
@@ -1,3 +1,29 @@
+#![allow(unused)]
+
+fn print_len(s: &str) {
+    println!("len: {}", s.len());
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Exercise 1
+    let s = String::from("Rust");
+    // let s1 = &mut s;
+    let s1 = &s;
+    // let s2 = &mut s;
+    let s2 = &s;
+
+    println!("s1: {s1}");
+    println!("s2: {s2}");
+
+    // Exercise 2
+    let mut s = String::from("Rust");
+    let s1 = &mut s;
+    // let s2 = &mut s;
+
+    s1.push_str("!!!");
+    println!("s: {s}");
+
+    // Exercise 3
+    let s = String::from("Rust");
+    print_len(&s);
 }

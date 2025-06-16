08a77e6433439c75d68f24311ea776e2ed8d57d6
t4sk <tsk.nakamura@gmail.com>
2025-06-17 07:54:28 +0900

    unwrap

---
+++ topics/unwrap/solutions/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn parse_and_add(a: &str, b: &str) -> u32 {
+    let x: u32 = a.parse().expect("Failed to parse variable a");
+    let y: u32 = b.parse().expect("Failed to parse variable b");
+    x + y
+}
+
+pub fn unwrap_and_add(x: Option<u32>, y: Option<u32>) -> u32 {
+    x.unwrap() + y.unwrap()
+}
---
+++ topics/unwrap/solutions/tests/test.rs
@@ -0,0 +1,26 @@
+use unwrap::*;
+
+#[test]
+fn test_parse_and_add() {
+    assert_eq!(parse_and_add("1", "2"), 3);
+}
+
+#[test]
+#[should_panic(expected = "Failed to parse variable a")]
+fn test_parse_and_add_panic_1() {
+    parse_and_add("a", "2");
+}
+
+#[test]
+#[should_panic(expected = "Failed to parse variable b")]
+fn test_parse_and_add_panic_2() {
+    parse_and_add("1", "b");
+}
+
+#[test]
+fn test_unwrap_and_add() {
+    let x = Some(1);
+    let y = Some(2);
+    assert_eq!(unwrap_and_add(x, y), 3);
+}
+

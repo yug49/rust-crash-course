cea74bc56210623dbefa2a0c8666b374eebc9179
t4sk <tsk.nakamura@gmail.com>
2025-06-16 13:40:02 +0900

    var solutions

---
+++ topics/variable/solutions/README.md
@@ -0,0 +1,19 @@
+# Variable
+
+## Example
+
+Execute the following command to run [`./examples/var.rs`]
+
+```shell
+cargo run --example var
+```
+
+## Exercise 1
+
+Make `count` into a mutable variable
+
+## Test
+
+```shell
+cargo run
+```
---
+++ topics/variable/solutions/src/main.rs
@@ -0,0 +1,10 @@
+#![allow(unused)]
+// Exercise: Fix the code to make it compile and pass the assertions
+
+fn main() {
+    // Exercise 1: Make this variable mutable
+    let mut count = 1;
+    count += 1;
+
+    println!("count: {count}");
+}

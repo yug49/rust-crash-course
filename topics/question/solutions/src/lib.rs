cf1b5601ba0839c4999a0df50f93f46bc6ccae54
t4sk <tsk.nakamura@gmail.com>
2025-06-17 08:01:46 +0900

    cargo

---
+++ topics/question/solutions/src/lib.rs
@@ -0,0 +1,16 @@
+fn parse(s: &str) -> Result<u32, String> {
+    match s.parse() {
+        Ok(val) => Ok(val),
+        Err(_) => Err("Failed to parse string into u32".to_string())
+    }
+}
+
+pub fn sum(nums: &[&str]) -> Result<u32, String> {
+    let mut total = 0;
+
+    for n in nums {
+        total += parse(n)?;
+    }
+
+    Ok(total)
+}

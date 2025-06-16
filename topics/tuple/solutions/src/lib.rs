ce8a0208744068ea5b2b641680e64b2b8d6659eb
t4sk <tsk.nakamura@gmail.com>
2025-06-16 20:00:02 +0900

    tuple

--- topics/tuple/solutions/src/lib.rs
+++ topics/tuple/solutions/src/lib.rs
@@ -1,11 +1,11 @@
 pub fn first(t: (bool, u32, char)) -> bool {
-    todo!();
+    t.0
 }
 
 pub fn last(t: (bool, u32, char)) -> char {
-    todo!();
+    t.2
 }
 
 pub fn swap(t: (u32, u32)) -> (u32, u32) {
-    todo!();
+    (t.1, t.0)
 }

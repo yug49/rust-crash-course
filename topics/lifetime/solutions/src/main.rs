6be70a4cc07ebd46c1e9a429c6194465fdd0440b
t4sk <tsk.nakamura@gmail.com>
2025-06-17 13:31:02 +0900

    lifetime

--- topics/lifetime/solutions/src/main.rs
+++ topics/lifetime/solutions/src/main.rs
@@ -1,3 +1,23 @@
+
+#[derive(Debug)]
+struct Config<'a> {
+    url: &'a str,
+    port: u32,
+    db_url: &'a str,
+}
+
+fn log(config: &Config) {
+    println!("Connecting to database at {}...", config.db_url);
+    println!("server listening at {}:{}...", config.url, config.port);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config {
+        url: "localhost",
+        port: 3000,
+        db_url: "db://localhost"
+    };
+
+    log(&config);
+
 }

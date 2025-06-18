6583cf04f47db341dfad0a4389b58e4e44e55bb6
t4sk <tsk.nakamura@gmail.com>
2025-06-18 12:35:38 +0900

    ex

---
+++ topics/modules/exercises/src/main.rs
@@ -0,0 +1,55 @@
+mod math {
+    pub fn min(x: u32, y: u32) -> u32 {
+        if x <= y {
+            x
+        } else {
+            y
+        }
+    }
+
+    pub fn max(x: u32, y: u32) -> u32 {
+        if x >= y {
+            x
+        } else {
+            y
+        }
+    }
+}
+
+mod util {
+    pub mod log {
+        pub fn debug(s: &str) {
+            println!("DEBUG: {s}");
+        }
+    }
+
+    pub mod vec {
+        pub fn first(v: &[u32]) -> Option<u32> {
+            let n = v.len();
+            if n > 0 {
+                Some(v[0])
+            } else {
+                None
+            }
+        }
+
+        pub fn last(v: &[u32]) -> Option<u32> {
+            let n = v.len();
+            if n > 0 {
+                Some(v[n - 1])
+            } else {
+                None
+            }
+        }
+    }
+
+}
+
+fn main() {
+    util::log::debug(
+        &format!("min: {}", math::min(1, 2))
+    );
+    util::log::debug(
+        &format!("max: {}", math::max(1, 2))
+    );
+}

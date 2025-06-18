00692f85243013d15b799afd9879d03ddeb18766
t4sk <tsk.nakamura@gmail.com>
2025-06-18 12:30:06 +0900

    ?

---
+++ topics/question/exercises/src/lib.rs
@@ -0,0 +1,10 @@
+fn parse(s: &str) -> Result<u32, String> {
+    match s.parse() {
+        Ok(val) => Ok(val),
+        Err(_) => Err("Failed to parse string into u32".to_string())
+    }
+}
+
+pub fn sum(nums: &[&str]) -> Result<u32, String> {
+    todo!();
+}
---
+++ topics/question/exercises/tests/test.rs
@@ -0,0 +1,11 @@
+use question::*;
+
+#[test]
+fn test_sum() {
+    let nums = vec!["1", "2", "3"];
+    assert_eq!(sum(&nums), Ok(6));
+
+    let nums = vec!["1", "2", "3", "a"];
+    assert_eq!(sum(&nums), Err("Failed to parse string into u32".to_string()));
+}
+

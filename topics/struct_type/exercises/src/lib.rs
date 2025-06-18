08bf9045851df8fbf72459be07a3c84278f96382
t4sk <tsk.nakamura@gmail.com>
2025-06-18 11:14:13 +0900

    struct

--- topics/struct_type/exercises/src/lib.rs
+++ topics/struct_type/exercises/src/lib.rs
@@ -1,9 +1,7 @@
 #[derive(Debug)]
 pub struct Account {
-    pub address: String,
-    pub balance: u32,
 }
 
 pub fn new(address: String) -> Account {
-    Account { address, balance: 0 }
+    todo!();
 }
